463359392fd6a38eb2cc39941a0767c7073907d4
Márton Schmalzl <schmalzl.marton@kodinformatika.hu>
2019-12-03 11:35:02 +0100

    day-1 part 2

--- src/day1/Fuel.scala
+++ src/day1/Fuel.scala
@@ -3,9 +3,21 @@ package day1
 import scala.io.Source
 
 object Fuel extends App {
-  println(System.getProperty("user.dir"))
-  println("Hello, World!")
   val bufferedSource = Source.fromFile("./assets/day1-1.txt")
 
   println(bufferedSource.getLines.map(line => line.toInt).fold(0)((acc, it) => acc + math.floor(it / 3) .toInt - 2))
+
+  val fuelCalc =  new ((Int) => Int) {
+    def apply(mass: Int):Int = {
+      val fuel:Int = math.floor(mass / 3).toInt - 2
+      if (fuel > 0) {
+        fuel + apply(fuel)
+      } else {
+        0
+      }
+    }
+  }
+  val bufferedSource2 = Source.fromFile("./assets/day1-1.txt")
+  println(bufferedSource2.getLines.map(line => line.toInt).fold(0)((acc, it) => acc + fuelCalc(it)))
+
 }
